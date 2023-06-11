35eaacec6bcac5270c80388d6e9ac306b12eca70
Kevin <kevin923.dai@gmail.com>
2023-06-10 20:43:54 -0500

    added footer and routes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -12,7 +14,7 @@ export class HeaderComponent {
   showAddTask: boolean = false;
   subscription: Subscription = new Subscription();
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService.onToggle().subscribe((bool) => this.showAddTask = bool);
   }
 
@@ -20,5 +22,9 @@ export class HeaderComponent {
 
   toggleAddTask(){
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string){
+    return this.router.url === route;
   }
 }
